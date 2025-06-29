5ac5e0a3a33890518272216313d66a96fc07c611
mohitrajput-7860 <mohitsingh5934@gmail.com>
2025-06-29 18:27:03 +0530

    arranged code

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Login from './components/Login';
+import Signup from './components/Signup';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="home" element={<h2 style={{ padding: "2rem" }}>🏠 Welcome Home!</h2>} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
